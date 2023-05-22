0593d3c0d928039d544e9b6fcdb996570dd94906
André <abs+github@emancipe.se>
2023-05-21 23:51:49 -0300

    Replace fprintf with portable l_sprintf in g_write function
    This commit updates the g_write function in the 'liolib.c' file to use the
    portable l_sprintf macro instead of fprintf for number-to-string conversion.
    This enhances portability and maintains consistency with the rest of the
    codebase which uses l_sprintf. The formatted string is then written to the file
    using fputs.

--- liolib.c
+++ liolib.c
@@ -662,12 +662,14 @@ static int g_write (lua_State *L, FILE *f, int arg) {
   int status = 1;
   for (; nargs--; arg++) {
     if (lua_type(L, arg) == LUA_TNUMBER) {
+      char buff[100];
       /* optimization: could be done exactly as for strings */
       int len = lua_isinteger(L, arg)
-                ? fprintf(f, LUA_INTEGER_FMT,
+                ? l_sprintf(buff, sizeof buff, LUA_INTEGER_FMT,
                              (LUAI_UACINT)lua_tointeger(L, arg))
-                : fprintf(f, LUA_NUMBER_FMT,
+                : l_sprintf(buff, sizeof buff, LUA_NUMBER_FMT,
                              (LUAI_UACNUMBER)lua_tonumber(L, arg));
+      fputs(buff, f);
       status = status && (len > 0);
     }
     else {
